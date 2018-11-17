e42bbc31ce561f0a3da643f4111affcaea745748
Nick Benoit <nbenoit3@gmail.com>
2018-11-17 12:15:41 -0500

    added the uber icon to the info window

--- googlemap/main.js
+++ googlemap/main.js
@@ -58,7 +58,7 @@ function callback(results, status) {
     }
   }
 
- // creating markers for render based of types  ex--> type: ['bar']
+//  creating markers for render based of types  ex--> type: ['bar']
 function createMarker(place){
     var placeLoc = place.geometry.location;
     marker = new google.maps.Marker({
@@ -66,14 +66,26 @@ function createMarker(place){
         position: placeLoc
     });
 
+    //This is the string that includes the name and the uber icon link.
+    var contentString = `
+        <div class="d-flex">
+            <p>${place.name}</p>
+        </div>
+        <div>
+        <a href="https://m.uber.com/looking" target="_blank"><img src="../images/uber-icon.png" width="20px" height="20px" display="inline"/></a>
+        </div>
+    `
+
+    //When you click on the marker, the uber icon appears in the info window. 
     //Name of location will hover over pin when clicked
     google.maps.event.addListener(marker, 'click', function(){
-        infowindow.setContent(place.name);
+        infowindow.setContent(contentString);
         infowindow.open(map, this);
     });
 
     return marker;
 }
+
 
 
 function clearResults(){
