6371bc666e33f3d52f944c8ea0105b3442a51335
Ron Cagle <ron@atlantashirt.com>
2018-11-23 21:51:23 -0500

    Styled the front page get lit button

--- googlemap/main.js
+++ googlemap/main.js
@@ -278,8 +278,8 @@ function createMarker(place){
             Price Level: ${place.price_level }
         </div>
         <div>
-        <a href="https://m.uber.com/looking" target="_blank"><img src="../images/uber-icon.png" width="20px" height="20px"/></a>
-        <a href="https://ride.lyft.com" target="_blank"><img src="../images/lyft-icon.png" width="30px" height="20px"/></a>
+        <a href="https://m.uber.com/looking" target="_blank" title="Summon Uber!"><img src="../images/uber-icon.png" title="Summon Uber" width="20px" height="20px"/></a>
+        <a href="https://ride.lyft.com" target="_blank" title="Need a Lyft?"><img src="../images/lyft-icon.png" width="30px" height="20px"/></a>
         </div>
     `
 
