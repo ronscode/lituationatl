78d50ce9973bd3b1a23b3aa743417508d06c64dc
Jeffrey-Hargis <hargis.jeffrey@gmail.com>
2018-11-27 20:57:35 -0500

    log in capable

--- login/login.js
+++ login/login.js
@@ -1,93 +1,49 @@
-function onSignIn(googleUser) {
-    // Useful data for client-side scripts:
-    var profile = googleUser.getBasicProfile();
-    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-    console.log('Full Name: ' + profile.getName());
-    console.log('Given Name: ' + profile.getGivenName());
-    console.log('Family Name: ' + profile.getFamilyName());
-    console.log("Image URL: " + profile.getImageUrl());
-    console.log("Email: " + profile.getEmail());
-
-    // The ID token you need to pass to your backend:
-    var id_token = googleUser.getAuthResponse().id_token;
-    postAJAX('/server/sign-in', {
-            id_token: id_token
-        })
-        .then(function (user) {
-            ux_mode = 'redirect',
-                redirect_uri = 'http://localhost:3000/googlemap/' + user.username
-        })
-
-
+var config = {
+    apiKey: "AIzaSyBS40ndyKqGN9X_7poexr8sEGNqH3BZOLo",
+    authDomain: "lituation-front-end-project.firebaseapp.com",
+    databaseURL: "https://lituation-front-end-project.firebaseio.com",
+    projectId: "lituation-front-end-project",
+    storageBucket: "lituation-front-end-project.appspot.com",
+    messagingSenderId: "504597097079"
 };
 
-function onSuccess(googleUser) {
-    console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
-}
+firebase.initializeApp(config);
 
-function onFailure(error) {
-    console.log(error);
-}
-//firebase login initiation tester code
-firebase.auth().onAuthStateChanged(function (user) {
-    if (user) {
-        // User is signed in.
-        var displayName = user.displayName;
-        var email = user.email;
-        var emailVerified = user.emailVerified;
-        var photoURL = user.photoURL;
-        var isAnonymous = user.isAnonymous;
-        var uid = user.uid;
-        var providerData = user.providerData;
-        // ...
-    } else {
-        // User is signed out.
-        // ...
-    }
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+ui.start('#firebaseui-auth-container', {
+    signInOptions: [
+        firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    // Other config options...
 });
 
-firebase.auth().createUserWithEmailAndPassword(email, password).catch(function (error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ...
-});
-
-firebase.auth().signInWithEmailAndPassword(email, password).catch(function (error) {
-    // Handle Errors here.
-    var errorCode = error.code;
-    var errorMessage = error.message;
-    // ...
-});
-
-//facebook shite/WILL ADD AFTER SITE GOES LIVE
-FB.getLoginStatus(function (response) {
-    statusChangeCallback(response);
-});
-
-function checkLoginState() {
-    FB.getLoginStatus(function (response) {
-        statusChangeCallback(response);
-    });
-}
-
-window.fbAsyncInit = function () {
-    FB.init({
-        appId: '{263581801174793}',
-        cookie: true,
-        xfbml: true,
-        version: '{1.0}'
-    });
-
-    FB.AppEvents.logPageView();
-
+var uiConfig = {
+    callbacks: {
+        signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+            return true;
+        },
+        uiShown: function () {
+            document.getElementById('loader').style.display = 'none';
+        }
+    },
+    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+    signInFlow: 'popup',
+    signInSuccessUrl: 'http://localhost:3000/googlemap/',
+    signInOptions: [
+        // Leave the lines as is for the providers you want to offer your users.
+        // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+        // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+        // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+        // firebase.auth.GithubAuthProvider.PROVIDER_ID,
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        // firebase.auth.PhoneAuthProvider.PROVIDER_ID
+    ],
+    // Terms of service url.
+    tosUrl: '',
+    // Privacy policy url.
+    privacyPolicyUrl: ' '
 };
 
-(function (d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s);
-    js.id = id;
-    js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&autoLogAppEvents=1&version=v3.2&appId=263581801174793';
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+
+ui.start('#firebaseui - auth - container', uiConfig);
